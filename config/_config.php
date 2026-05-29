39e6f65e78ec08cd5f497e996742713c2a147e8f
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2026-05-29 14:38:52 +0200

    Fix correct indentation.

--- config/_config.php
+++ config/_config.php
@@ -1,12 +1,12 @@
 <?php
 
-session_start();
+    session_start();
 
-define('TEMPLATE_VIEW_PATH', './views/templates/');
-define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
+    define('TEMPLATE_VIEW_PATH', './views/templates/');
+    define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
 
-//Exemple de configuration
-define('DB_HOST', 'your_host');
-define('DB_NAME', 'your_database');
-define('DB_USER', 'your_user');
-define('DB_PASS', 'your_password');
+    //Exemple de configuration
+    define('DB_HOST', 'your_host');
+    define('DB_NAME', 'your_database');
+    define('DB_USER', 'your_user');
+    define('DB_PASS', 'your_password');
