66ffbf8ae952c8badd0ee776c0c4c009a6b5ec69
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2026-05-29 14:22:53 +0200

    Remove local config file from repository

--- config/_config.php
+++ config/_config.php
@@ -1,17 +1,12 @@
 <?php
-    
-    // En fonction des routes utilisées, il est possible d'avoir besoin de la session ; on la démarre dans tous les cas. 
-    session_start();
 
-    // Ici on met les constantes utiles, 
-    // les données de connexions à la bdd
-    // et tout ce qui sert à configurer. 
+session_start();
 
-    define('TEMPLATE_VIEW_PATH', './views/templates/'); // Le chemin vers les templates de vues.
-    define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php'); // Le chemin vers le template principal.
+define('TEMPLATE_VIEW_PATH', './views/templates/');
+define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
 
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'blog_forteroche');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-
+//Exemple de configuration
+define('DB_HOST', 'your_host');
+define('DB_NAME', 'your_database');
+define('DB_USER', 'your_user');
+define('DB_PASS', 'your_password');
